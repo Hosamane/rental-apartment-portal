fa21bbffcc4c623efa004732191078ee144426d3
Hosamane Veerabhadrappa Setty <140387249+Hosamane@users.noreply.github.com>
2026-02-16 16:41:02 +0530

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,8 +1,20 @@
 
+# import os
+
+# class Config:
+#     SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@db:5432/rental_db"
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     JWT_SECRET_KEY = "super-secret-key"
+
+
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@db:5432/rental_db"
+    uri = os.getenv("DATABASE_URL")
+
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = "super-secret-key"
 
