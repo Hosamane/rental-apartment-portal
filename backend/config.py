5ef8f9064fa6bc61c088e69743f3de351b1573ed
Hosamane Veerabhadrappa Setty <140387249+Hosamane@users.noreply.github.com>
2026-02-16 16:33:45 +0530

    Update config.py
    railway db

--- backend/config.py
+++ backend/config.py
@@ -6,3 +6,11 @@ class Config:
         "postgresql://postgres:rmRvKicgQOKfloTywFxHOfBibypuopoS@postgres.railway.internal:5432/railway"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+uri = os.getenv("DATABASE_URL")
+
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = uri
