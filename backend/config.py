d472c24433766b1def73beae2454e4e5ac33ef3b
Hosamane Veerabhadrappa Setty <140387249+Hosamane@users.noreply.github.com>
2026-02-16 17:09:40 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,12 +1,10 @@
 import os
 
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 uri = os.getenv("DATABASE_URL")
 
 if uri and uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = uri
+class Config:
+    SQLALCHEMY_DATABASE_URI = uri
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
