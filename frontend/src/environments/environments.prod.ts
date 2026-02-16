61fe3190bdb76e1f775ba36eb0447e549680bb5e
Hosamane Veerabhadrappa Setty <140387249+Hosamane@users.noreply.github.com>
2026-02-16 18:06:14 +0530

    Update environments.prod.ts

--- frontend/src/environments/environments.prod.ts
+++ frontend/src/environments/environments.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://rental-apartment-portal-production.up.railway.app/api'
+  apiUrl: 'https://rental-apartment-portal-production.up.railway.app'
 };
