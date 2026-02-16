0342482b4e8f7926cf6490f4a2e005890d045ab3
Hosamane Veerabhadrappa Setty <140387249+Hosamane@users.noreply.github.com>
2026-02-16 16:53:04 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "postgresql://postgres:rmRvKicgQOKfloTywFxHOfBibypuopoS@postgres.railway.internal:5432/railway"
+        "postgresql://postgres:MwOMSMcEfBRJYSeXAUVljNlakNZccDTq@postgres.railway.internal:5432/railway"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
